5349a5bed6dfcc3b45d52f2c55a19cffc406d08a
Premanand Chowdhury <freudigprem@gmail.com>
2023-01-09 11:59:42 +0000

    Added basic routes using React Router V5

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,45 @@
 import './App.css';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
 import Navbar from './components/navbar/Navbar';
 
 import Home from './pages/home/Home'
+import Register from './pages/register/Register'
+import Login from './pages/login/Login'
+import Create from './pages/create/Create';
+import Settings from './pages/settings/Settings'
+import Single from './pages/single/Single';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
+    <Router>
       <Navbar />
-      <Home />
-    </div>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/create">
+          <Create />
+        </Route>
+        <Route path="/settings">
+          <Settings />
+        </Route>
+        <Route path="/post/:postId">
+          <Single />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
