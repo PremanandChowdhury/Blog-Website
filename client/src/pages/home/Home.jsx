f446f37039d38a0621bc9c5d0730d9fed20ddaaa
Premanand Chowdhury <freudigprem@gmail.com>
2023-01-09 07:01:58 +0000

    Created Homepage

---
+++ client/src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../../components/header/Header'
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      Home Page
+    </div>
+  )
+}
+
+export default Home
