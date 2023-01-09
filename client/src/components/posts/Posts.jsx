1974a50a83896bba428f5a7ae96f3746f09f6b97
Premanand Chowdhury <freudigprem@gmail.com>
2023-01-09 08:00:15 +0000

    Added Posts Component

---
+++ client/src/components/posts/Posts.jsx
@@ -0,0 +1,20 @@
+import './Posts.css'
+
+import Post from '../post/Post'
+
+const Posts = () => {
+  return (
+    <div className="posts">
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+    </div>
+  )
+}
+
+export default Posts
