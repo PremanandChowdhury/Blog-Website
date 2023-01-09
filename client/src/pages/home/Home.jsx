8b24746942ae8359cd1e8d95f82c8bf07f46edbe
Premanand Chowdhury <freudigprem@gmail.com>
2023-01-09 12:29:25 +0000

    Some changes

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import './Home.css'
+
 import Header from '../../components/header/Header'
+import Posts from '../../components/posts/Posts'
+import Sidebar from '../../components/sidebar/Sidebar'
 
 const Home = () => {
   return (
-    <div>
+    <>
       <Header />
-      Home Page
-    </div>
+      <div className="home">
+        <Posts />
+        <Sidebar />
+      </div>
+    </>
   )
 }
 
