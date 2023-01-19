56cedf354acf2dab974dce876823b2089f630078
Premanand Chowdhury <freudigprem@gmail.com>
2023-01-19 11:30:59 +0000

    Create a Post Handling data

--- client/src/pages/create/Create.jsx
+++ client/src/pages/create/Create.jsx
@@ -1,29 +1,99 @@
 import './Create.css'
 
+import { useContext, useState } from 'react'
+import axios from 'axios'
+
+import {Context} from '../../context/Context'
+
 const Create = () => {
+  const [title, setTitle] = useState("")
+  const [description, setDescription] = useState("")
+  const [file, setFile] = useState(null)
+
+  const {user} = useContext(Context)
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const newPost = {
+      title,
+      description,
+      username: user.username,
+    }
+
+    console.log('newPost', newPost);
+
+    if(file) {
+      const data = new FormData()
+      const filename = Date.now() + "/" + file.name
+      data.append("name", filename)
+      data.append("file", file)
+      newPost.photo = filename
+
+      try {
+        await axios.post("/upload", data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    try {
+      const response = await axios.post("/posts", newPost)
+      // console.log('>> response', response);
+      window.location.replace("/posts/" + response.data._id)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <div className='create'>
-      <img 
-        className='createImg'
-        src="https://images.pexels.com/photos/371909/pexels-photo-371909.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Post Cover" />
-      <form className='createForm'>
+      {file && (
+        <img 
+          className='createImg'
+          src={URL.createObjectURL(file)} 
+          alt="Post Cover" />
+      )}
+      <form className='createForm' onSubmit={handleSubmit}>
         <div className='createFormGroup'>
           <label htmlFor='fileInput' >
             <i className="createIcon fas fa-plus"></i>
           </label>
-          <input type='file' id='fileInput' style={{display: "none"}}/>
-          <input type="text" placeholder='Title' className='createInput' autoFocus={true} />
+          <input 
+            type='file' 
+            id='fileInput' 
+            style={{display: "none"}} 
+            onChange={
+              (e) => setFile(e.target.files[0])
+              }
+          />
+          <input 
+            type="text" 
+            placeholder='Title' 
+            className='createInput' 
+            autoFocus={true} 
+            onChange = {
+              (e) => setTitle(e.target.value)
+              }
+          />
         </div>
 
         <div className="createFormGroup">
           <textarea 
             placeholder='Write your content here....'
             type='text'
-            className='createInput createText' cols="30" rows="10">
+            className='createInput createText' 
+            cols="30" 
+            rows="10"
+            onChange={
+              (e) => setDescription(e.target.value)
+            }
+          >
           </textarea>
         </div>
 
-        <button className="createSubmit">Publish</button>
+        <button className="createSubmit" type="submit">Publish</button>
       </form>
     </div>
   )
